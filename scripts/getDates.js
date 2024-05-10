a420cba4d9b1518442324e9594fb70c18f524ef4
Axizerq <axizer0123@mail.ru>
2024-05-09 23:30:19 -0600

    week02

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -11,3 +11,32 @@ yearElement.textContent = currentYear;
 // Получаем и выводим дату и время последнего изменения документа
 const lastModified = document.lastModified;
 lastModifiedElement.textContent = lastModified;
+
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#000";
+        main.style.color = "#00FF00";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
