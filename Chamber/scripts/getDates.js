fd7d1c847f94a85d6c96bb0f51a5435a889bc5a9
Axizerq <axizer0123@mail.ru>
2024-06-06 01:08:58 -0600

    fix

--- Chamber/scripts/getDates.js
+++ Chamber/scripts/getDates.js
@@ -26,3 +26,16 @@ hambutton.addEventListener('click', () => {
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('timestamp').value = new Date().toISOString();
 });
+
+
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
