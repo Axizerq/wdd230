7ba67e645fb7d6923a77ab3717dcea30bc334a38
Axizerq <axizer0123@mail.ru>
2024-06-06 01:18:49 -0600

    fix

--- Chamber/scripts/getDates.js
+++ Chamber/scripts/getDates.js
@@ -14,7 +14,7 @@ lastModifiedElement.textContent = lastModified;
 
 
 
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.nav')
 const hambutton = document.querySelector('#menu');
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
@@ -25,17 +25,4 @@ hambutton.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('timestamp').value = new Date().toISOString();
-});
-
-
-
-
-// Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
 });
